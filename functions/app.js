89c271f7345f72dafcb9e4ff7863c34032c228d5
Tokinantenaina <tokinantenainaraboba@gmail.com>
2025-09-17 19:11:59 +0300

    app.ts

--- functions/app.js
+++ functions/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import serverless from "serverless-http";
-import testHandler from "../test.ts"; // ajuste le chemin si nécessaire
+// import testHandler from "../test.ts"; // ajuste le chemin si nécessaire
+import api_start from "../src/app.ts"; // ajuste le chemin si nécessaire
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/.netlify/functions/app", testHandler);
+// app.use("/.netlify/functions/app", testHandler);
+app.use("/.netlify/functions/app", api_start);
 
 export const handler = serverless(app);

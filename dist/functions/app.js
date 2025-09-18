e615d1c49074b7154417fcb0a9beb2165ce8c782
Tokinantenaina <tokinantenainaraboba@gmail.com>
2025-09-18 09:34:24 +0300

    correction de dist/redirect

--- dist/functions/app.js
+++ dist/functions/app.js
@@ -1,19 +1,5 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const app = require("../app");
 const serverless = require("serverless-http");
-const app = require("../app"); // Import de votre app Express
 
-// Création d'une instance Express pour Netlify
-const netlifyApp = express();
-
-// Middleware
-netlifyApp.use(bodyParser.json());
-netlifyApp.use(express.urlencoded({ extended: true }));
-netlifyApp.use(cors());
-
-// Montez votre application principale sur le chemin Netlify
-netlifyApp.use("/.netlify/functions/app", app.default || app);
-
-// Handler pour Netlify Functions
-exports.handler = serverless(netlifyApp);
+// Handler Netlify Functions
+exports.handler = serverless(app.default || app);
